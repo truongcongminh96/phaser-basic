00c80b924089da00f70f1557aae60c3ab4aefcda
Minh Truong <truongcongminh96@gmail.com>
2021-03-25 10:11:25 +0700

    Update recycle pipes

--- src/index.js
+++ src/index.js
@@ -25,11 +25,10 @@ const VELOCITY = 200;
 const PIPES_TO_RENDER = 4;
 
 let bird = null;
-let upperPipe = null;
-let lowerPipe = null;
-let pipeHorizontalDistance = 0;
+let pipes = null;
 
-let pipeVerticalOpeningDistanceRange = [150, 250];
+const pipeVerticalDistanceRange = [150, 250];
+const pipeHorizontalDistanceRange = [500, 550];
 
 const flapVelocity = 150;
 const initPosition = {x: config.width * 0.1, y: config.height / 2};
@@ -46,17 +45,16 @@ function create() {
     bird = this.physics.add.sprite(initPosition.x, initPosition.y, 'bird').setOrigin(0);
     bird.body.gravity.y = 400;
 
+    pipes = this.physics.add.group();
+
     for (let i = 0; i < PIPES_TO_RENDER; i++) {
-        pipeHorizontalDistance += 400;
-        let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalOpeningDistanceRange);
-        let piperVerticalPosition = Phaser.Math.Between(20, config.height - 20 - pipeVerticalDistance);
+        const upperPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 1);
+        const lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 0);
 
-        upperPipe = this.physics.add.sprite(pipeHorizontalDistance, piperVerticalPosition, 'pipe').setOrigin(0, 1);
-        lowerPipe = this.physics.add.sprite(upperPipe.x, upperPipe.y + pipeVerticalDistance, 'pipe').setOrigin(0, 0);
-
-        upperPipe.body.velocity.x = -200;
-        lowerPipe.body.velocity.x = -200;
+        placePipe(upperPipe, lowerPipe);
     }
+
+    pipes.setVelocityX(-200);
 
     this.input.on('pointerdown', flap);
 
@@ -69,6 +67,43 @@ function update(time, delta) {
     if (bird.y > config.height || bird.y < 0 - bird.height) {
         restartBirdPosition();
     }
+
+    recyclePipes();
+}
+
+function placePipe(uPipe, lPipe) {
+    const rightMostX = getRightMostPipe();
+    const pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+    const pipeVerticalPosition = Phaser.Math.Between(20, config.height - 20 - pipeVerticalDistance);
+    const pipeHorizontalDistance = Phaser.Math.Between(...pipeHorizontalDistanceRange);
+
+    uPipe.x = rightMostX + pipeHorizontalDistance;
+    uPipe.y = pipeVerticalPosition;
+
+    lPipe.x = uPipe.x;
+    lPipe.y = uPipe.y + pipeVerticalDistance;
+}
+
+function recyclePipes() {
+    const tempPipes = [];
+    pipes.getChildren().forEach(pipe => {
+        if (pipe.getBounds().right <= 0) {
+            tempPipes.push(pipe);
+            if (tempPipes.length === 2) {
+                placePipe(...tempPipes);
+            }
+        }
+    })
+}
+
+function getRightMostPipe() {
+    let rightMostX = 0;
+
+    pipes.getChildren().forEach(function (pipe) {
+        rightMostX = Math.max(pipe.x, rightMostX);
+    });
+
+    return rightMostX;
 }
 
 function restartBirdPosition() {
