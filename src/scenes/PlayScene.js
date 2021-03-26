d01da79ca87202e7b844bece8f24fcaf90170e6c
Minh Truong <truongcongminh96@gmail.com>
2021-03-26 11:05:16 +0700

    pause game physics

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -109,9 +109,11 @@ class PlayScene extends Phaser.Scene {
     }
 
     gameOver() {
-        this.bird.x = this.config.startPosition.x;
-        this.bird.y = this.config.startPosition.y;
-        this.bird.body.velocity.y = 0;
+        // this.bird.x = this.config.startPosition.x;
+        // this.bird.y = this.config.startPosition.y;
+        // this.bird.body.velocity.y = 0;
+        this.physics.pause();
+        this.bird.setTint(0xEE4824);
     }
 
     flap() {
