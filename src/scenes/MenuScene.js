41996e9470b84b62e7d716bf558a0aff2b66a8e4
Minh Truong <truongcongminh96@gmail.com>
2021-03-26 14:58:12 +0700

    Add Navigate to screens

--- src/scenes/MenuScene.js
+++ src/scenes/MenuScene.js
@@ -15,7 +15,28 @@ class MenuScene extends BaseScene {
     create() {
         super.create();
 
-        this.createMenu(this.menu);
+        this.createMenu(this.menu, this.setupMenuEvents.bind(this));
+    }
+
+    setupMenuEvents(menuItem) {
+        const textGO = menuItem.textGO;
+        textGO.setInteractive();
+
+        textGO.on('pointerover', () => {
+            textGO.setStyle({fill: '#FF0'})
+        });
+
+        textGO.on('pointerout', () => {
+            textGO.setStyle({fill: '#FFF'})
+        });
+
+        textGO.on('pointerup', () => {
+            menuItem.scene && this.scene.start(menuItem.scene);
+
+            if (menuItem.text === 'Exit') {
+                this.game.destroy(true);
+            }
+        });
     }
 }
 
