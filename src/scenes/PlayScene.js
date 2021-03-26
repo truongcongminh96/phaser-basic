efe729bc6071e0be9b3592c5b251117436b78327
Minh Truong <truongcongminh96@gmail.com>
2021-03-26 15:51:09 +0700

    Add count down resume event

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -27,11 +27,39 @@ class PlayScene extends BaseScene {
         this.createScore();
         this.createPause();
         this.handleInputs();
+        this.listenToEvents();
     }
 
     update() {
         this.checkGameStatus();
         this.recyclePipes();
+    }
+
+    listenToEvents() {
+        if (this.pauseEvent) {
+            return;
+        }
+        this.pauseEvent = this.events.on('resume', () => {
+            this.initialTime = 3;
+            this.countDownText = this.add.text(...this.screenCenter, 'Fly in: ' + this.initialTime, this.fontOptions).setOrigin(0.5);
+            this.timedEvent = this.time.addEvent({
+                delay: 1000,
+                callback: this.countDown,
+                callbackScope: this,
+                loop: true
+            })
+        })
+    }
+
+    countDown() {
+        this.initialTime--;
+        this.countDownText.setText('Fly in: ' + this.initialTime);
+
+        if (this.initialTime <= 0) {
+            this.countDownText.setText('');
+            this.physics.resume();
+            this.timedEvent.remove();
+        }
     }
 
     createBG() {
@@ -163,7 +191,7 @@ class PlayScene extends BaseScene {
 
     increaseScore() {
         this.score++;
-        this.scoreText.setText(`Score: ${this.score}`)
+        this.scoreText.setText(`Score: ${this.score}`);
     }
 }
 
