abc2ccc8884f2a6e38fbf45dc17dabbb88b223ae
Minh Truong <truongcongminh96@gmail.com>
2021-03-26 13:25:00 +0700

    Pausing the game

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -24,6 +24,7 @@ class PlayScene extends Phaser.Scene {
         this.load.image('sky', 'assets/sky.png');
         this.load.image('bird', 'assets/bird.png');
         this.load.image('pipe', 'assets/pipe.png');
+        this.load.image('pause', 'assets/pause.png');
     }
 
     create() {
@@ -32,6 +33,7 @@ class PlayScene extends Phaser.Scene {
         this.createPipes();
         this.createColliders();
         this.createScore();
+        this.createPause();
         this.handleInputs();
     }
 
@@ -76,6 +78,15 @@ class PlayScene extends Phaser.Scene {
         const bestScore = localStorage.getItem('bestScore');
         this.scoreText = this.add.text(16, 16, `Score: ${this.score}`, {fontSize: '32px', fill: '#000'});
         this.add.text(16, 52, `Best score: ${bestScore || 0}`, {fontSize: '18px', fill: '#000'});
+    }
+
+    createPause() {
+        const pauseButton = this.add.image(this.config.width - 10, this.config.height - 10, 'pause').setInteractive().setScale(3).setOrigin(1);
+
+        pauseButton.on('pointerdown', () => {
+            this.physics.pause();
+            this.scene.pause();
+        });
     }
 
     handleInputs() {
