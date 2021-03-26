8a734000b9e809e541f89d6689af28ebb5074142
Minh Truong <truongcongminh96@gmail.com>
2021-03-26 14:14:48 +0700

    Menu items

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -1,18 +1,21 @@
 import Phaser from 'phaser';
 
 class PreloadScene extends Phaser.Scene {
-    constructor(config) {
+
+    constructor() {
         super('PreloadScene');
-        this.config = config;
     }
 
     preload() {
         this.load.image('sky', 'assets/sky.png');
+        this.load.image('bird', 'assets/bird.png');
+        this.load.image('pipe', 'assets/pipe.png');
+        this.load.image('pause', 'assets/pause.png');
     }
 
     create() {
-        this.add.image(0, 0, 'sky').setOrigin(0);
+        this.scene.start('MenuScene');
     }
 }
 
-export default PreloadScene
+export default PreloadScene;
