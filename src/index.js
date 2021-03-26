264fe2b17751d5d1adeb9c031b4e825c8b66a81a
Minh Truong <truongcongminh96@gmail.com>
2021-03-26 09:27:50 +0700

    Shared config

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import Phaser from "phaser";
-import PlayScene from "./scenes/PlayScene";
+import Phaser from 'phaser';
+import PlayScene from './scenes/PlayScene';
+
+const WIDTH = 800;
+const HEIGHT = 600;
+const BIRD_POSITION = {x: WIDTH * 0.1, y: HEIGHT / 2};
+
+const SHARED_CONFIG = {
+    width: WIDTH,
+    height: HEIGHT,
+    startPosition: BIRD_POSITION
+};
 
 const config = {
-    // WebGL (Web graphics library)
+    // WebGL (Web graphics library) JS Api for rendering 2D and 3D graphics
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    ...SHARED_CONFIG,
     physics: {
         // Arcade physics plugin, manages physics simulation
         default: 'arcade',
@@ -13,10 +22,8 @@ const config = {
             debug: true,
         }
     },
-    scene: [PlayScene]
+    scene: [new PlayScene(SHARED_CONFIG)]
 };
-
-new Phaser.Game(config);
 
 const VELOCITY = 200;
 const PIPES_TO_RENDER = 4;
@@ -24,13 +31,14 @@ const PIPES_TO_RENDER = 4;
 let bird = null;
 let pipes = null;
 
+let pipeHorizontalDistance = 0;
+
 const pipeVerticalDistanceRange = [150, 250];
 const pipeHorizontalDistanceRange = [500, 550];
 
-const flapVelocity = 150;
-const initPosition = {x: config.width * 0.1, y: config.height / 2};
+const flapVelocity = 250;
+const initalBirdPosition = {x: config.width * 0.1, y: config.height / 2}
 
-// Loading assets, such as images, music, animations,...
 function preload() {
     this.load.image('sky', 'assets/sky.png');
     this.load.image('bird', 'assets/bird.png');
@@ -39,7 +47,7 @@ function preload() {
 
 function create() {
     this.add.image(0, 0, 'sky').setOrigin(0);
-    bird = this.physics.add.sprite(initPosition.x, initPosition.y, 'bird').setOrigin(0);
+    bird = this.physics.add.sprite(initalBirdPosition.x, initalBirdPosition.y, 'bird').setOrigin(0);
     bird.body.gravity.y = 400;
 
     pipes = this.physics.add.group();
@@ -48,20 +56,19 @@ function create() {
         const upperPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 1);
         const lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 0);
 
-        placePipe(upperPipe, lowerPipe);
+        placePipe(upperPipe, lowerPipe)
     }
 
     pipes.setVelocityX(-200);
 
     this.input.on('pointerdown', flap);
-
-    this.input.keyboard.on('keyup', flap);
+    this.input.keyboard.on('keydown_SPACE', flap);
 }
 
-// if bird position x is same or larger than width of canvas go back to left
-// if bird position x is smaller or equal to 0 then move back the right
+// if bird y position is small than 0 or greater than height of the canvas
+// then alert "you have lost"
 function update(time, delta) {
-    if (bird.y > config.height || bird.y < 0 - bird.height) {
+    if (bird.y > config.height || bird.y < -bird.height) {
         restartBirdPosition();
     }
 
@@ -71,14 +78,14 @@ function update(time, delta) {
 function placePipe(uPipe, lPipe) {
     const rightMostX = getRightMostPipe();
     const pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
-    const pipeVerticalPosition = Phaser.Math.Between(20, config.height - 20 - pipeVerticalDistance);
+    const pipeVerticalPosition = Phaser.Math.Between(0 + 20, config.height - 20 - pipeVerticalDistance);
     const pipeHorizontalDistance = Phaser.Math.Between(...pipeHorizontalDistanceRange);
 
     uPipe.x = rightMostX + pipeHorizontalDistance;
     uPipe.y = pipeVerticalPosition;
 
     lPipe.x = uPipe.x;
-    lPipe.y = uPipe.y + pipeVerticalDistance;
+    lPipe.y = uPipe.y + pipeVerticalDistance
 }
 
 function recyclePipes() {
@@ -98,17 +105,20 @@ function getRightMostPipe() {
 
     pipes.getChildren().forEach(function (pipe) {
         rightMostX = Math.max(pipe.x, rightMostX);
-    });
+    })
 
     return rightMostX;
 }
 
 function restartBirdPosition() {
-    bird.x = initPosition.x;
-    bird.y = initPosition.y;
+    bird.x = initalBirdPosition.x;
+    bird.y = initalBirdPosition.y;
     bird.body.velocity.y = 0;
 }
 
 function flap() {
     bird.body.velocity.y = -flapVelocity;
 }
+
+
+new Phaser.Game(config);
