0777393b266ecffedc42cbb19798864c4afc6472
Minh Truong <truongcongminh96@gmail.com>
2021-03-26 15:23:26 +0700

    Add pause scene

---
+++ src/scenes/PauseScene.js
@@ -0,0 +1,38 @@
+import BaseScene from './BaseScene';
+
+class PauseScene extends BaseScene {
+
+    constructor(config) {
+        super('PauseScene', config);
+
+        this.menu = [
+            {scene: 'PlayScene', text: 'Continue'},
+            {scene: 'MenuScene', text: 'Exit'},
+        ]
+    }
+
+    create() {
+        super.create();
+
+        this.createMenu(this.menu, this.setupMenuEvents.bind(this));
+    }
+
+    setupMenuEvents(menuItem) {
+        const textGO = menuItem.textGO;
+        textGO.setInteractive();
+
+        textGO.on('pointerover', () => {
+            textGO.setStyle({fill: '#FF0'})
+        });
+
+        textGO.on('pointerout', () => {
+            textGO.setStyle({fill: '#FFF'})
+        });
+
+        textGO.on('pointerup', () => {
+
+        });
+    }
+}
+
+export default PauseScene;
